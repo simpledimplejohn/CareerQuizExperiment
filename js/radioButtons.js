4437ba5d7d74405ffd2795ba8e43aeb4a49f68bf
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-13 07:41:16 -0700

    debug test form, now able to print to the DOM

---
+++ js/radioButtons.js
@@ -0,0 +1,30 @@
+console.log("help!")
+
+$(document).ready(function() {
+  $('#radio-form').submit(function(event) {
+    event.preventDefault();
+    const q1 = $("input:radio[name=q1]:checked").val();
+    const q2 =$("input:radio[name=q2]:checked").val();
+    const q3 =$("input:radio[name=q3]:checked").val();
+    let answersArray = [q1,q2,q3]
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    for(let char of answersArray) {
+      if(char === "a") {
+        a ++
+      }
+      if(char === "b") {
+        b ++
+      }
+      if(char === "c") {
+        c ++
+      }
+    }
+    a = Math.round((a/8)*100)
+    b = Math.round((b/8)*100)
+    c = Math.round((c/8)*100)
+    console.log(a, b, c)
+    $("#output").html(`<p><em>your percentage of a answers is: ${a}%<em><p>`)
+  })
+})
